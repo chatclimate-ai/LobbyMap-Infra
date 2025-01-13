8d739b36810a4ab3dac39fda04b29e8d41586d94
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2025-01-13 13:59:45 +0100

    schemas

---
+++ lobby-map-fbtool/lobby-map-fbtool/api/schema.py
@@ -0,0 +1,137 @@
+from pydantic import BaseModel, Field
+from typing import Optional, Literal, List
+
+
+class EvidenceSearchModel(BaseModel):
+    searched_query: str = Field(
+        ...,
+        description="The query that was used to search for evidence",
+    )
+    searched_author: Optional[str] = Field(
+        default=None,
+        description="The author that was used to search for evidence",
+    )
+    searched_date: Optional[str] = Field(
+        default=None,
+        description="The date that was used to search for evidence",
+    )
+    searched_region: Optional[str] = Field(
+        default=None,
+        description="The region that was used to search for evidence",
+    )
+    searched_file_name: Optional[str] = Field(
+        default=None,
+        description="The file name that was used to search for evidence",
+    )
+    top_k: int = Field(
+        ...,
+        description="The number of top evidences to be retrieved",
+    )
+
+
+class ParserModel(BaseModel):
+    model_name: str = Field(
+        ...,
+        description="The name of the parser model",
+    )
+    options: Optional[dict] = Field(
+        default=None,
+        description="The options of the parser model",
+    )
+
+class RAGComponents(BaseModel):
+    chunking_method: str = Field(
+        ...,
+        description="The method used to chunk the evidence",
+    )
+    chunking_similarity_threshold: float = Field(
+        ...,
+        description="The similarity threshold used to chunk the evidence",
+    )
+    vectorizer_model_name: str = Field(
+        ...,
+        description="The name of the vectorizer model",
+    )
+    reranker_model_name: str = Field(
+        ...,
+        description="The name of the reranker model",
+    )
+
+class Artifacts(BaseModel):
+    parser: ParserModel
+    rag_components: RAGComponents
+class EvidenceModel(BaseModel):
+    """
+    The model class that represents the chunk evidence to be sent back
+    """
+    search_elements: EvidenceSearchModel = Field(
+        ...,
+        description="The search elements that were used to retrieve the evidence",
+    )
+    pdf_doc_name: str = Field(
+        ...,
+        description="The name of the pdf document that the evidence was retrieved from",
+    )
+    content: str = Field(
+        ...,
+        description="The content of the evidence",
+    )
+    author: Optional[str] = Field(
+        default=None,
+        description="The name of the company that the evidence was retrieved from",
+    )
+    date: Optional[str] = Field(
+        default=None,
+        description="The applied to the retrieved evidence",
+    )
+    region: Optional[str] = Field(
+        default=None,
+        description="The region where the evidence comes from",
+    )
+    rank: int = Field(
+        ...,
+        description="The rank of the evidence among the retrieved evidences, coming from RAG",
+    )
+    status: Literal['approved', 'rejected'] = 'approved'
+    confidence_score: float = Field(
+        ...,
+        description="The confidence score of the evidence coming from the embedding model",
+    )
+    rank_score: float = Field(
+        ...,
+        description="The rank score of the evidence coming from the ranker model",
+    )
+    generated_stance: int = Field(
+        ...,
+        description="The stance generated for the evidence by the LLM model",
+    )
+    generated_stance_reason: str = Field(
+        ...,
+        description="The reason for the stance generated for the evidence by the LLM model",
+    )
+    generated_stance_score: float = Field(
+        ...,
+        description="The stance score generated for the evidence by the LLM model",
+    )
+    updated_rank: int = Field(
+        ...,
+        description="The updated rank after feedback",
+    )
+    updated_status: Optional[Literal['approved', 'rejected']] = None
+    updated_generated_stance: Optional[int] = None
+    timestamp: str = Field(
+        ...,
+        description="The timestamp of the feedback",
+    )
+    artifacts: Artifacts = Field(
+        ...,
+        description="The artifacts used in the RAG engine",
+    )
+
+
+class FeedbackResponseModel(EvidenceModel):
+    id: str
+
+
+class FeedbackCollection(BaseModel):
+    feedbacks: List[FeedbackResponseModel]
