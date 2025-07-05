af19f4d8a23f0a56b275a02fe422fee94163f54f
imene-swaan <imenekolli@outlook.com>
2025-07-05 17:46:12 +0200

    use latest nomic

--- ollama/pull-models.sh
+++ ollama/pull-models.sh
@@ -7,8 +7,8 @@ ollama serve &
 echo ">>>>>>>> Wait a few seconds for Ollama to spin up..."
 sleep 5
 
-echo ">>>>>>>> Pulling nomic-embed-text:v1.5 and qwen3:1.7b models..."
-ollama pull nomic-embed-text:v1.5
+echo ">>>>>>>> Pulling nomic-embed-text:latest and qwen3:1.7b models..."
+ollama pull nomic-embed-text:latest
 ollama pull qwen3:1.7b
 
 # Keep container alive by waiting on the Ollama serve process
